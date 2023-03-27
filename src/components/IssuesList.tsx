fb514cfec842a761231cb28c67a5b4b8ffc5bb2f
Andrey Baglay <andreybaglai@gmail.com>
2023-03-27 16:36:29 +0300

    add issue list component layout

--- src/components/IssuesList.tsx
+++ src/components/IssuesList.tsx
@@ -1,15 +1,90 @@
-import React from "react";
+import React, { FC } from "react";
+import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 
-const IssuesList = () => {
+interface IssueListItem {
+  assignee: null;
+  comments: string[];
+  completedDate: null;
+  createdBy: string;
+  createdDate: string;
+  dueDate: string;
+  id: string;
+  labels: string[];
+  number: number;
+  title: string;
+  status?: string;
+}
+
+interface IssueListItemProps {
+  title: string;
+  number: number;
+  assignee: null;
+  commentCount: number;
+  createdBy: string;
+  createdDate: string;
+  labels: string[];
+  status: string | undefined;
+}
+
+const IssuesListItem: FC<IssueListItemProps> = ({
+  title,
+  number,
+  assignee,
+  commentCount,
+  createdBy,
+  createdDate,
+  labels,
+  status,
+}) => {
+  return (
+    <li>
+      <div>{status === "done" || status === "cancelled"}</div>
+    </li>
+  );
+};
+
+const IssuesList: FC = () => {
+  const issuesQuery = useQuery(["labels"], () =>
+    fetch(`${import.meta.env.VITE_API_URL}/issues`).then((res) => res.json())
+  );
+
+  console.log(issuesQuery);
+
   return (
     <div>
-      <h1>Issues List</h1>
-      <ul>
-        <li>
-          <Link to="/issue/1">Issue 1</Link>
-        </li>
-      </ul>
+      <h2>Issues List</h2>
+      {issuesQuery.isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {issuesQuery?.data?.map(
+            ({
+              id,
+              title,
+              number,
+              assignee,
+              createdBy,
+              createdDate,
+              labels,
+              status,
+              comments,
+            }: IssueListItem) => (
+              <IssuesListItem
+                key={id}
+                title={title}
+                number={number}
+                assignee={assignee}
+                commentCount={comments.length}
+                createdBy={createdBy}
+                createdDate={createdDate}
+                labels={labels}
+                status={status}
+              />
+            )
+          )}
+        </ul>
+      )}
     </div>
   );
 };
