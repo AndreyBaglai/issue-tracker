d3de8bbac8dfbe42d8d155729afb79debd4058d7
Andrey Baglay <andreybaglai@gmail.com>
2023-03-27 14:38:07 +0300

    add react query provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
+
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <QueryClientProvider value={queryClient}>
+    <App />
+  </QueryClientProvider>
 );
